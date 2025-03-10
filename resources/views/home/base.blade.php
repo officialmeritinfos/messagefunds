67d792c6e9fe57b8dfbf4ad7898ee3f9900a6da5
Anon Dev <michaelerastus9@gmail.com>
2025-03-10 07:10:26 +0100

    Removed old support

--- resources/views/home/base.blade.php
+++ resources/views/home/base.blade.php
@@ -549,17 +549,6 @@
 <!-- end popup massage -->
 @stack('js')
 
-<!-- Smartsupp Live Chat script -->
-<script type="text/javascript">
-    var _smartsupp = _smartsupp || {};
-    _smartsupp.key = '927c132e3e1f36de7de9d069c3d5d165edfa57bc';
-    window.smartsupp||(function(d) {
-        var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
-        s=d.getElementsByTagName('script')[0];c=d.createElement('script');
-        c.type='text/javascript';c.charset='utf-8';c.async=true;
-        c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
-    })(document);
-</script>
-<noscript> Powered by <a href=“https://www.smartsupp.com” target=“_blank”>Smartsupp</a></noscript>
+
 </body>
 </html>
